a6a2fd31a8bebd9a90b16509971cf4359800f18b
d0x471b <0x471@protonmail.com>
2022-12-02 02:51:26 +0300

    🗺️ Add fully fledged example template (#4)

--- examples/index.js
+++ examples/index.js
@@ -13,9 +13,10 @@ createServer({
 }).then((vite) => {
   const app = express()
   app.use(vite.middlewares)
+  console.log("path:", import.meta.url);
   app.use("/", (req, res, next) => {
     console.log(req.path);
-    let page = readFileSync('examples/index.html', 'utf-8');
+    let page = readFileSync('examples/simple-example/index.html', 'utf-8');
     vite.transformIndexHtml(req.path, page).then((page) => {
       res.status(200)
         .set({ 'Content-type': 'text/html;charset=utf-8' })
---
+++ examples/simple-example/index.js
@@ -0,0 +1,7 @@
+
+import Cüzdan from './wallet'
+import dom from '../../lib/util/dom'
+
+
+console.log(dom.adla("na").onclick)
+dom.adla("na").onclick = Cüzdan.bağla();
